44e6e3893be5f60984c4c71cedc1686273de8e07
Ramin Yaghoubzadeh <ryaghoub@techfak.uni-bielefeld.de>
2012-08-15 10:37:19 +0200

    Updated resolver

--- resolve.sh
+++ resolve.sh
@@ -2,16 +2,31 @@
 
 PACKAGES="rsb"
 
+shopt -q nullglob || resetnullglob=1
+shopt -s nullglob
+shopt -q dotglob || resetdotglob=1
+shopt -s dotglob
+
 mkdir -p deps/bin
 mkdir -p deps/lib
 mkdir -p deps/include
 mkdir -p deps/scripts
 mkdir -p deps/python
 for P in $PACKAGES; do
-	cp -a ../$P/dist/bin/* deps/bin/
-	cp -a ../$P/dist/lib/* deps/lib/
-	cp -a ../$P/dist/include/* deps/include/
-	cp -a ../$P/dist/scripts/* deps/scripts/
-	cp -a ../$P/dist/python/* deps/python/
+	files=(../$P/dist/bin/*);
+	[ "$files" ] && cp -a ../$P/dist/bin/* deps/bin/
+	files=(../$P/dist/lib/*);
+	[ "$files" ] && cp -a ../$P/dist/lib/* deps/lib/
+	files=(../$P/dist/include/*);
+	[ "$files" ] && cp -a ../$P/dist/include/* deps/include/
+	files=(../$P/dist/scripts/*);
+	[ "$files" ] && cp -a ../$P/dist/scripts/* deps/scripts/
+	files=(../$P/dist/python/*.zip);
+	[ "$files" ] && for zipfile in ../$P/dist/python/*.zip; do
+		unzip -qq $zipfile -d deps/python
+	done
 done
+
+[ "$resetdotglob" ] && shopt -u dotglob
+[ "$resetnullglob" ] && shopt -u nullglob
 
