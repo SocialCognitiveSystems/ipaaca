b3d333728984d779f36420b644727e0e3004b8ed
Ramin Yaghoubzadeh <ryaghoub@techfak.uni-bielefeld.de>
2013-02-28 17:12:44 +0100

    Updated ipaaca helper scripts:
    ipaaca-iu-sniffer optionally takes a list of category arguments
    (if not specified, it still listens for everything).
    ipaaca-iu-injector needs to be provided with a category name
    and, optionally, with key-value pairs for the payload, as such:
    $ ipaaca-iu-injector  murmlfile  data  smile_3sec.us

--- ipaacatools/scripts/ipaaca-iu-injector.py
+++ ipaacatools/scripts/ipaaca-iu-injector.py
@@ -3,18 +3,33 @@
 import time
 import logging
 import ipaaca
+import sys
 
 def my_update_handler(iu, event_type, local):
 	print(event_type+': '+str(iu))
 
-ob = ipaaca.OutputBuffer('CoolInformerOut')
+if len(sys.argv)<2:
+	print "Please use the program as follows:"
+	print " "+sys.argv[0]+" <categoryname> [<payloadkey> <payloadvalue>] [<k2> <v2>] ..."
+	sys.exit(1)
+
+cate = sys.argv[1]
+idx=2
+pl={}
+while len(sys.argv)>idx+1:
+	pl[sys.argv[idx]] = sys.argv[idx+1]
+	idx+=2
+
+print "Sending Message of category "+cate
+print " with payload "+str(pl)
+
+ob = ipaaca.OutputBuffer('IUInjector')
 ob.register_handler(my_update_handler)
-
-iu_top = ipaaca.IU('murmlfile')
-iu_top.payload = {'data': 'HalloTest.us'}
-#iu_top.payload = {'data': 'smile_7sec.us'}
+iu_top = ipaaca.Message(cate)
+iu_top.payload = pl
 ob.add(iu_top)
+print "Message sent."
 
-time.sleep(1)
-print "Done"
+time.sleep(0.3)
+print "done."
 
--- ipaacatools/scripts/ipaaca-iu-sniffer.py
+++ ipaacatools/scripts/ipaaca-iu-sniffer.py
@@ -3,16 +3,21 @@
 import time
 import logging
 import ipaaca
+import sys
 
 def my_update_handler(iu, event_type, local):
 	t=time.localtime()
 	print str(t.tm_hour)+':'+str(t.tm_min)+':'+str(t.tm_sec),
 	print(event_type+': '+unicode(iu))
 
-ib = ipaaca.InputBuffer('SnifferIn', [''])
+cats = []
+if len(sys.argv)>1:
+	cats = sys.argv[1:]
+
+ib = ipaaca.InputBuffer('SnifferIn', [''] if len(cats)==0 else cats)
 ib.register_handler(my_update_handler)
 
-print("Listening for IU events of any category...")
+print("Listening for IU events of "+("any category..." if len(cats)==0 else "categories: "+' '.join(cats)))
 print('')
 while True:
 	time.sleep(1)
