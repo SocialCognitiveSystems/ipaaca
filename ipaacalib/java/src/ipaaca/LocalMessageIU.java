92fd84c6315729ecdf66b060bcd908aeab534681
Hendrik Buschmeier <hbuschme@uni-bielefeld.de>
2013-06-20 16:27:50 +0200

    Added constructors to LocalMessageIU.

--- ipaacalib/java/src/ipaaca/LocalMessageIU.java
+++ ipaacalib/java/src/ipaaca/LocalMessageIU.java
@@ -1,10 +1,21 @@
 package ipaaca;
 
+
 /**
  * Local IU of Message sub-type. Can be handled like a normal IU, but on the remote side it is only existent during the handler calls.
  * @author hvanwelbergen
  */
 public class LocalMessageIU extends LocalIU
 {
+
+    public LocalMessageIU()
+    {
+        super();
+    }
+
+    public LocalMessageIU(String category)
+    {
+        super(category);
+    }
 
 }
